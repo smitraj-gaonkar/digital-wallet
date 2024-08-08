a0b91c3fe34dc6f390d4fd4e94c05c27f1547c43
smitrajgaonkar7@gmail.com <smitrajgaonkar7@gmail.com>
2024-08-08 19:43:28 +0530

    first commit

---
+++ src/main/java/com/example/wallet/controller/BaseController.java
@@ -0,0 +1,16 @@
+package com.example.wallet.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class BaseController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> baseContext() {
+        return new ResponseEntity<>("Digital Wallet", HttpStatus.OK);
+    }
+    
+}
